30e0563fa8df9627767a5e092805e53ca38138c7
Ezra Shim <ezrashim@gmail.com>
2016-04-02 23:50:12 -0400

    create config

--- app/controllers/basic_configurations_controller.rb
+++ app/controllers/basic_configurations_controller.rb
@@ -4,4 +4,29 @@ class BasicConfigurationsController < ApplicationController
     @amps_configuration = AmpsConfiguration.new
     @origin_experiment = OriginExperiment.new
   end
+
+  def create
+    binding.pry
+    @basic_configuration = BasicConfiguration.new(basic_configuration_params)
+
+    if @basic_configuration.save
+      redirect_to root_path
+    else
+      flash.now[:notice] = @basic_configuration.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def basic_configuration_params
+    params.require(:basic_configuration).permit(
+      :site_name,
+      :status,
+      :site_url,
+      :variation,
+      :default_behavior,
+      :enable_detect_variation,
+      :enable_AMPS,
+      :enable_origin_experiements
+    )
+  end
 end
